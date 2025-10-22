563300bdf2d599a7ee141f950897cd5e16ee4f46
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-10-22 09:32:18 +0200

    page structure

---
+++ src/app/components/lostpass/lostpass.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-lostpass',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './lostpass.component.html',
+  styleUrl: './lostpass.component.scss'
+})
+export class LostpassComponent {
+  send(){
+    
+  }
+}
